16eaae33708089efba80681840b1e19c37c33e79
brianbok <brianbok97@gmail.com>
2025-04-08 22:08:02 +0900

    Feat: Service findById 구현체 메서드 작성
    club array에 값을 할당하고 참조하는 역할의 Service 구현체작성
    배열로 구현된 자료를 탐색하는 방식으로 구현

--- src/io/junesoo/java/travelClub/service/logic/ClubServiceLogic.java
+++ src/io/junesoo/java/travelClub/service/logic/ClubServiceLogic.java
@@ -26,6 +26,7 @@ public class ClubServiceLogic implements ClubService {
     @Override
     public TravelClub[] findAll() {
         // copy 를 이용하여 배열에 존재하는 데이터만 return
+        // iterable 하게 만들어서 확인한다.
         return Arrays.copyOfRange(clubs, 0,index);
     }
 
@@ -36,8 +37,31 @@ public class ClubServiceLogic implements ClubService {
 
     @Override
     public TravelClub findById(String clubId) {
-        return null;
+        int length_ = clubs.length;
+        TravelClub foundClub = null;
+        
+        for(int i=0; i<length_ ;i++){
+            if (clubs[i].getId().equals(clubId)) {
+                foundClub = clubs[i];
+                break;
+            }
+        }
+        return foundClub;
     }
+// 다른 방법
+//    public TravelClub findById(String clubId){
+//        // iterable 하게 초기화 
+//        TravelClub[] createdClubs = Arrays.copyOfRange(clubs, 0,index);
+//        TravelClub foundClub = null;
+//        for(TravelClub club : createdClubs){ // 모든 요소 탐색
+//            if (club.getId().equals(clubId)){
+//                foundClub = club;
+//                break;
+//            }
+//        }
+//        return foundClub;
+//    }
+
 
     @Override
     public void modify(TravelClub modifyClub) {
--- src/io/junesoo/java/travelClub/ui/console/ClubConsole.java
+++ src/io/junesoo/java/travelClub/ui/console/ClubConsole.java
@@ -34,11 +34,13 @@ public class ClubConsole {
             TravelClub newClub = new TravelClub(clubName,intro);
             // 배열에 저장 - clubService 로직
             clubService.register(newClub);
+            System.out.println("Registered Club :" + newClub.toString());
         }
     }
 
     public void findAll(){
-        //
+        // 왜 꼭 ClubServiceLogic 을 따로 구현해서 쓸까?
+        // 리스트라는 데이터에 접근하는 것은 Service 만을 이용하겠다는 "모듈화"!
         TravelClub[] foundClubs = clubService.findAll();
         // 만들어진 클럽이 없다면
         if(foundClubs.length == 0) {
@@ -52,6 +54,24 @@ public class ClubConsole {
     }
 
     public void findById() {
+        // 초기화 대상인 변수는 null 로 설정
+        TravelClub foundClub = null;
+        while (true) {
+            String clubId = consoleUtil.getValueOf("Club id to find(0.Club Menu)");
+            if(clubId.equals("0")) {
+                break;
+            }
+            foundClub = clubService.findById(clubId);
+
+            if (foundClub != null) {
+                System.out.println(foundClub.toString());
+            } else {
+                System.out.println("Can not find club Id : " + clubId);
+            }
+        }
+    }
+
+    public void findByName() {
 
     }
 }
